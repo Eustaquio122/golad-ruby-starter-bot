3f846609248b1f4765768513420d3a9eb517d571
Eustaquio122 <tiagofsousa@gmail.com>
2018-01-15 13:18:07 +0000

    Refactors/Adds Comments to main

---
+++ main.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+require_relative 'lib/bot/bot'
+require_relative 'lib/bot/formatter'
+require_relative 'lib/bot/state/state'
+require_relative 'lib/bot/state/player'
+require_relative 'lib/action/action'
+require_relative 'lib/action/clock'
+require_relative 'lib/action/moves/random_move'
+
+
+def main
+  $stdout.sync = true # sets up immediate output flush
+  Bot.new.run
+end
+
+main

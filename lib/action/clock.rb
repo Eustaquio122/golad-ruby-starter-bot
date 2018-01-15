fa54842607bd48dfa99fe8856e566047b6a622fc
Eustaquio122 <tiagofsousa@gmail.com>
2018-01-15 13:20:23 +0000

    Refactors/Adds Comments to clock

---
+++ lib/action/clock.rb
@@ -0,0 +1,18 @@
+class Clock
+  def start
+    reset # clears @in and @out
+    @in = Process.clock_gettime(Process::CLOCK_MONOTONIC) # starts clock by storing starting time in @in
+  end
+
+  def stop #stops clock and returns timelapse in ms
+    @out = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+    (@out - @in) * 1000
+  end
+
+  private
+
+  def reset
+    @in = nil
+    @out = nil
+  end
+end

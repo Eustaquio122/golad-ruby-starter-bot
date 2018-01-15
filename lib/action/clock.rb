dfd7ffaabffde0148d9790a7e2dc7aae83a0fa70
Eustaquio122 <tiagofsousa@gmail.com>
2018-01-15 14:56:26 +0000

    Corrects comment indentation in clock

--- lib/action/clock.rb
+++ lib/action/clock.rb
@@ -4,7 +4,7 @@ class Clock
     @in = Process.clock_gettime(Process::CLOCK_MONOTONIC) # starts clock by storing starting time in @in
   end
 
-  def stop #stops clock and returns timelapse in ms
+  def stop # stops clock and returns timelapse in ms
     @out = Process.clock_gettime(Process::CLOCK_MONOTONIC)
     (@out - @in) * 1000
   end
